defbbc3b54060c2de20656479a841d4ed8f8a31e
GaneshVarupula <132882854+GaneshVarupula@users.noreply.github.com>
2023-05-13 15:30:51 +0530

    Add files via upload

---
+++ details/details.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent {
+
+  myStyles={
+    'color':'red',
+    'background-color':'blue',
+    'font-size':'70px'
+  }
+
+}
